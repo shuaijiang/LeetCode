05e5cdf4ce9e719fd3f6938e2443cdaac266107d
shuaijiang <zhaoshuaijiang@sina.com>
2015-03-30 22:28:11 +0800

    Compare Version Numbers

--- CompareVersionNumbers.cpp
+++ CompareVersionNumbers.cpp
@@ -4,6 +4,8 @@
 *Email: zhaoshuaijiang8@gmail.com
 */
 #include<iostream>
+#include<sstream>
+#include<vector>
 #include<string.h>
 #include<math.h>
 using namespace std;
@@ -11,93 +13,99 @@ using namespace std;
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-		string v1_norm = removeBeginZeros(version1);
-		string v2_norm = removeBeginZeros(version2);
-		int num1_integer = str2IntegerPart(v1_norm);
-		int num1_decimal = str2DecimalPart(v1_norm);
-		int num2_integer = str2IntegerPart(v2_norm);
-		int num2_decimal = str2DecimalPart(v2_norm);
-		cout<<"num1="<<num1_integer<<"."<<num1_decimal<<endl;
-		cout<<"num2="<<num2_integer<<"."<<num2_decimal<<endl;
-    	if(num1_integer>num2_integer)
+		vector<string> vec1 = str2Vector(version1);
+		vector<string> vec2 = str2Vector(version2);
+		int strLarger;
+		int addSize = 0;
+		if(vec1.size() > vec2.size())
+		{
+			addSize = vec1.size() - vec2.size();
+			vectorRaise(vec2,addSize); 
+		}
+		else if(vec1.size() < vec2.size())
+		{
+			addSize = vec2.size() - vec1.size();
+			vectorRaise(vec1,addSize); 
+		}
+		vector<string>::iterator iter1,iter2;
+		for(iter1=vec1.begin(),iter2=vec2.begin();iter1<vec1.end()&&iter2<vec2.end();++iter1,++iter2)
+		{
+			strLarger = strCompare(*iter1,*iter2);
+			if(strLarger == 1)
+				return 1;
+			else if(strLarger == -1)
+				return -1;
+			else if(iter1==vec1.end()-1 && iter2==vec2.end()-1)
+			{
+				if(strLarger == 0)
+					return 0;
+			}
+		} 
+    }
+    int strCompare(string str1, string str2)
+    {
+    	int num1=str2Integer(str1);
+		int num2=str2Integer(str2);
+		if(num1>num2)
 			return 1;
-		else if(num1_integer<num2_integer)
+		else if(num1<num2)
 			return -1;
 		else
-		{
-			if(num1_decimal > num2_decimal)
-				return 1;
-			else if(num1_decimal < num2_decimal)
-				return -1;
-			else
-				return 0;
-		}
+			return 0;
     }
-    int str2IntegerPart(string str)
+    int str2Integer(string str)
     {
-		int integer_or_decimal = 1;
-		int integer_count = 0;
-		int num = 0;
+    	stringstream ss;
+    	int num;
+    	ss.clear();
+    	ss.str("");
+    	ss<<str;
+    	ss>>num;
+    	return num;
+    }
+    vector<string> str2Vector(string str)
+    {
+		string strNum, strTemp;
+		stringstream ss;
+		vector<string> strVec;
 		for(int count=0;count<str.size();++count)
 		{
-			
+			if(count == str.size()-1)
+			{
+				ss.clear();
+				ss<<str[count];
+				ss>>strTemp;
+				strNum.append(strTemp);
+				strVec.push_back(strNum);
+			}
 			if(str[count] == '.')
 			{
-				break;
+				strVec.push_back(strNum);
+				strNum.clear();
 			}
 			else
 			{
-				if(integer_or_decimal == 1)
-				{
-					integer_count ++;
-					num = num * pow(10.0,integer_count-1)+ (str[count]-'0');
-				}
+				ss.clear(); 
+				ss<<str[count];
+				ss>>strTemp;
+				strNum.append(strTemp);
 			}
 		}
-		return num;
+		return strVec;
     }
-	int str2DecimalPart(string str)
-    {
-		int isDecimal = 0;
-		int decimal_count = 0;
-		int num = 0;
-		for(int count=0;count<str.size();++count)
-		{
-			
-			if(str[count] == '.')
-				isDecimal = 1;
-			else
-			{
-				if(isDecimal == 1)
-				{
-					decimal_count ++;
-					num = num * pow(10.0,decimal_count-1) + (str[count]-'0');
-				}
-			}
-		}
-		return num;
-    }
-	string removeBeginZeros(string str)
+	void vectorRaise(vector<string> & vec,int addSize)
 	{
-		int begin_zero = 1;
-		string str_norm;
-		for(int count=0;count<str.size();++count)
-		{
-			if(str[count] != '0')
-				begin_zero = 0;
-			if(begin_zero == 0)
-				str_norm.push_back(str[count]);
-		}
-		return str_norm;
+		for(int count=0;count<addSize;count++)
+			vec.push_back("0");
 	}
 };
 
 int main()
 {
-	string str1 = "1.1";
-	string str2 = "1.10";
+	string str1 = "1.0";
+	string str2 = "0.1";
 	Solution s;
 	int res = s.compareVersion(str1,str2);
-	cout<<res<<endl;
-	system("pause");
+	cout<<"res="<<res<<endl;
+	//system("pause");
 }
