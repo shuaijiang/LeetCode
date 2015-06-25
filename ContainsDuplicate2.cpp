762ac6a077e058aab1911ea82b8cd7ca925fac02
shuaijiang <zhaoshuaijiang@sina.com>
2015-06-25 20:05:41 +0800

    Contains Duplicate2

---
+++ ContainsDuplicate2.cpp
@@ -0,0 +1,33 @@
+/*
+*Contains Duplicate2
+*Author: shuaijiang
+*Email: zhaoshuaijiang8@gmail.com
+*/
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string.h>
+#include<stdlib.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+    	if(nums.size()<=1)
+    		return false;
+        map<int,int> myMap;
+        map<int,int>::iterator mapIter;
+        for(int count=0;count<nums.size();++count){
+        	if(myMap.find(nums[count])==myMap.end())
+        		myMap[nums[count]] = count;
+        	else{
+        		if(count - myMap[nums[count]] <= k);
+        			return true;
+        		else
+        			myMap[nums[count]] = count;
+        	}
+        		
+        }
+        return false;
+    }
+};
