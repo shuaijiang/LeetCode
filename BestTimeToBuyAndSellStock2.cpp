b90cc294afd7543f60e624a47c3e95112a81f8c7
shuaijiang <zhaoshuaijiang@sina.com>
2015-07-09 22:08:25 +0800

    Best Time To Buy And Sell Stock2

---
+++ BestTimeToBuyAndSellStock2.cpp
@@ -0,0 +1,24 @@
+/*Maximum Subarray 
+*Author: shuaijiang
+*Email: zhaoshuaijiang8@gmail.com
+*/
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+       if(prices.size()<=0)
+       	return 0;
+	   int profit = 0;
+
+	   for(int i=1;i<prices.size();i++){
+	   		int diff = prices[i]-prices[i-1];
+	   		if(diff>=0)
+	   			profit += diff;
+	   }
+	   		
+	   	return profit;
+    }
+};
