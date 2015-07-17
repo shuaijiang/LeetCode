fde3475f8d82bd01e18f53a74d870600a6a3c6bb
shuaijiang <zhaoshuaijiang@sina.com>
2015-07-17 09:41:21 +0800

    Populating Next Right Pointers In Each Node2

---
+++ PopulatingNextRightPointersInEachNode2.cpp
@@ -0,0 +1,41 @@
+/*
+*Populating Next Right Pointers in Each Node 2
+*Author: shuaijiang
+*Email: zhaoshuaijiang8@gmail.com
+*/
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+using namespace std;
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+    	if(root == NULL)
+    		return;
+        vector<TreeLinkNode*> currLevel;
+        vector<TreeLinkNode*> nextLevel;
+        currLevel.push_back(root);
+        while(currLevel.size()>0){
+        	for(int i=0;i<currLevel.size();i++){
+	        	TreeLinkNode * node = currLevel[i];
+	        	if(i+1<currLevel.size())
+	        		node->next = currLevel[i+1];
+	        	if(node->left != NULL)
+					nextLevel.push_back(node->left);
+	        	if(node->right != NULL)
+	        		nextLevel.push_back(node->right);
+	        }
+	        currLevel = nextLevel;
+	        nextLevel.clear();
+        }
+    }
+};
